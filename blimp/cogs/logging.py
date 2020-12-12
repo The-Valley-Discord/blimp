f9a25f8b249d6242363b28adea18a6c0234f3893
Cassidy Dingenskirchen <admin@15318.de>
2020-12-12 16:49:58 +0100

    More logging

--- blimp/cogs/logging.py
+++ blimp/cogs/logging.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 
-from ..customizations import Blimp, Unauthorized
+from ..customizations import Blimp, UnableToComply, Unauthorized
 from .alias import MaybeAliasedTextChannel
 
 
@@ -38,4 +38,54 @@ class Logging(Blimp.Cog):
             },
         )
 
-        await ctx.reply(f"*New logs will be posted in {channel.mention}.*")
+        await ctx.reply(f"New logs will be posted in {channel.mention}.")
+
+    @commands.command(parent=logging)
+    async def disable(self, ctx: Blimp.Context):
+        "Disable logging and delete existing configuration."
+        if not ctx.privileged_modify(ctx.guild):
+            raise Unauthorized()
+
+        old = ctx.database.execute(
+            "SELECT * FROM logging_configuration WHERE guild_oid =:guild_oid",
+            {
+                "guild_oid": ctx.objects.make_object(g=ctx.guild.id),
+            },
+        ).fetchone()
+        if not old:
+            raise UnableToComply("Logging is not enabled in this server.")
+
+        await ctx.bot.post_log(
+            ctx.guild,
+            f"{ctx.author} disabled logging.",
+            color=ctx.Color.I_GUESS,
+        )
+
+        ctx.database.execute(
+            "DELETE FROM logging_configuration WHERE guild_oid =:guild_oid",
+            {
+                "guild_oid": ctx.objects.make_object(g=ctx.guild.id),
+            },
+        )
+
+        await ctx.reply("Disabled logging.")
+
+    @commands.command(parent=logging)
+    async def view(self, ctx: Blimp.Context):
+        "View the logging channel."
+
+        row = ctx.database.execute(
+            "SELECT * FROM logging_configuration WHERE guild_oid=:guild_oid",
+            {
+                "guild_oid": ctx.objects.make_object(g=ctx.guild.id),
+            },
+        ).fetchone()
+
+        if row:
+            channel = ctx.objects.by_oid(row["channel_oid"])["tc"]
+            await ctx.reply(f"The current logging channel is <#{channel}>.")
+        else:
+            await ctx.reply(
+                "There is no logging channel configured on this server.",
+                color=ctx.Color.I_GUESS,
+            )
