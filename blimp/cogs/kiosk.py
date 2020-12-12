3dc9a1b99737b55a9ea77f511b08de9a512dc3b5
Cassidy Dingenskirchen <admin@15318.de>
2020-12-12 17:08:15 +0100

    Refactor kiosk, add kiosk view

--- blimp/cogs/kiosk.py
+++ blimp/cogs/kiosk.py
@@ -1,6 +1,6 @@
 import json
 import re
-from typing import List, Union
+from typing import List, Tuple, Union
 
 import discord
 from discord.ext import commands
@@ -34,6 +34,26 @@ class Kiosk(Blimp.Cog):
 
         return result
 
+    def render_emoji_pairs(
+        self, pairs: List[Tuple[Union[int, str], Union[int, discord.Role]]], separator: str
+    ) -> str:
+        """Turn the stored data into a human-readable representation.
+        
+        Expects any combination of [("unicode_emoji", <Role Object>), (custom emoji id, role id)]"""
+
+        result = []
+        for maybe_emoji, maybe_role in pairs:
+            emoji = maybe_emoji
+            if isinstance(emoji, int):
+                emoji = self.bot.get_emoji(maybe_emoji) or (
+                    "<:emoji:" + str(maybe_emoji) + ">"
+                )
+            role_id = getattr(maybe_role, "id", maybe_role)
+
+            result.append(f"{emoji} <@&{role_id}>")
+
+        return separator.join(result)
+
     @commands.command(parent=kiosk)
     async def update(
         self,
@@ -42,7 +62,7 @@ class Kiosk(Blimp.Cog):
         args: commands.Greedy[Union[discord.Role, str]],
     ):
         """
-        Update a role kiosk, overwriting its setup entirely.
+        Update (or create) a role kiosk, overwriting its setup entirely.
 
         `args` is a space-separated list of one emoji each followed by one role. This determines the
         options the kiosk will have available. Due to Discord limitations, only 20 pairs are
@@ -111,19 +131,12 @@ class Kiosk(Blimp.Cog):
         if old:
             log_embed.add_field(
                 name="Old",
-                value="\n".join(
-                    [
-                        f"{ctx.bot.get_emoji(d[0]) or d[0]} <@&{d[1]}>"
-                        for d in json.loads(old["data"])
-                    ]
-                ),
+                value=self.render_emoji_pairs(json.loads(old["data"]), "\n"),
             )
 
         log_embed.add_field(
             name="New",
-            value="\n".join(
-                [f"{ctx.bot.get_emoji(d[0]) or d[0]} <@&{d[1]}>" for d in result]
-            ),
+            value=self.render_emoji_pairs(result, "\n"),
         )
 
         ctx.database.execute(
@@ -170,14 +183,10 @@ class Kiosk(Blimp.Cog):
                 + " update` first.",
             )
 
-        pairs = json.loads(old["data"]) + self.parse_emoji_pairs(args)
-
-        pair_string = " ".join(
-            [
-                f"{ctx.bot.get_emoji(d[0]) or d[0]} <@&{getattr(d[1], 'id', d[1])}>"
-                for d in pairs
-            ]
-            )
+        pair_string = self.render_emoji_pairs(
+            json.loads(old["data"]) + self.parse_emoji_pairs(args),
+            " ",
+        )
 
         text = f"kiosk{ctx.bot.suffix} update {msg.channel.id}-{msg.id} {pair_string}"
 
@@ -185,54 +194,28 @@ class Kiosk(Blimp.Cog):
             text,
             subtitle="Automagically invoking this command…",
             color=ctx.Color.AUTOMATIC_BLUE,
-            )
+        )
 
         async with ctx.typing():
             await ctx.invoke_command(text)
 
-        result = [(emoji, role.id) for (emoji, role) in result]
+    @commands.command(parent=kiosk)
+    async def view(self, ctx: Blimp.Context, msg: MaybeAliasedMessage):
+        """View the current configuration of a Kiosk.
 
-        for emoji in [item for item in args if item.__class__ == str]:
-            await msg.add_reaction(emoji)
+        `msg` is the message the Kiosk you're interested in is attached to."""
 
-        joined = old_data + result
-
-        log_embed = discord.Embed(
-            description=f"{ctx.author} updated "
-            f"[role kiosk in #{msg.channel.name}]({msg.jump_url}).",
-            color=ctx.Color.I_GUESS,
-        )
-
-        log_embed.add_field(
-            name="Old",
-            value="\n".join(
-                [
-                    f"{ctx.bot.get_emoji(d[0]) or d[0]} <@&{d[1]}>"
-                    for d in json.loads(old["data"])
-                ]
-            ),
-        )
-
-        log_embed.add_field(
-            name="New",
-            value="\n".join(
-                [f"{ctx.bot.get_emoji(d[0]) or d[0]} <@&{d[1]}>" for d in joined]
-            ),
-        )
-
-        ctx.database.execute(
-            "INSERT OR REPLACE INTO rolekiosk_entries(oid, data) VALUES(:oid,json(:data))",
-            {
-                "oid": ctx.objects.make_object(m=[msg.channel.id, msg.id]),
-                "data": json.dumps(joined),
-            },
-        )
-
-        await ctx.bot.post_log(msg.guild, embed=log_embed)
-
-        await ctx.reply(
-            f"*Appended to [role kiosk in #{msg.channel.name}]({msg.jump_url}).*"
-        )
+        row = ctx.database.execute(
+            "SELECT * FROM rolekiosk_entries WHERE oid=:oid",
+            {"oid": ctx.objects.by_data(m=[msg.channel.id, msg.id])},
+        ).fetchone()
+        if row:
+            await ctx.reply(
+                f"**[Role kiosk in #{msg.channel.name}]({msg.jump_url})**\n"
+                + self.render_emoji_pairs(json.loads(row["data"]), "\n"),
+            )
+        else:
+            raise UnableToComply("That message is not a Kiosk.")
 
     @commands.command(parent=kiosk)
     async def delete(
